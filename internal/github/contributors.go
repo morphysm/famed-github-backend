10a0a6781c30a81758975cefe46b297c38fd5ba9
Kilian Käslin <kilian.kaeslin@googlemail.com>
2022-01-19 16:35:43 +0100

    - Added event request concurrency

--- internal/github/contributors.go
+++ internal/github/contributors.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"sort"
+	"sync"
 
 	"github.com/google/go-github/v41/github"
 	"github.com/labstack/echo/v4"
@@ -35,12 +36,22 @@ func (gH *githubHandler) GetContributors(c echo.Context) error {
 	return c.JSON(http.StatusOK, contributors)
 }
 
+type eventContainer struct {
+	mu     sync.Mutex
+	events map[int64][]*github.IssueEvent
+}
+
+func (eC *eventContainer) add(key int64, events []*github.IssueEvent) {
+	eC.mu.Lock()
+	defer eC.mu.Unlock()
+	eC.events[key] = events
+}
+
 // issuesToContributors generates a contributor list based on a list of issues
 func (gH *githubHandler) issuesToContributors(ctx context.Context, issues []*github.Issue, repoName string) ([]*kudo.Contributor, error) {
 	var (
 		contributorsArray = []*kudo.Contributor{}
 		filteredIssues    []*github.Issue
-		eventsByIssue     = map[int64][]*github.IssueEvent{}
 	)
 
 	if len(issues) == 0 {
@@ -58,17 +69,14 @@ func (gH *githubHandler) issuesToContributors(ctx context.Context, issues []*git
 			continue
 		}
 		filteredIssues = append(filteredIssues, issue)
-
-		// TODO add concurrency
-		eventsResp, err := gH.githubInstallationClient.GetIssueEvents(ctx, repoName, *issue.Number)
-		if err != nil {
-			return nil, err
-		}
-
-		eventsByIssue[*issue.ID] = eventsResp
 	}
 
-	contributors := kudo.GenerateContributors(filteredIssues, eventsByIssue, gH.kudoRewardUnit, gH.kudoRewards, usdToEthRate)
+	events, err := gH.getEvents(ctx, filteredIssues, repoName)
+	if err != nil {
+		return nil, err
+	}
+
+	contributors := kudo.GenerateContributors(filteredIssues, events, gH.kudoRewardUnit, gH.kudoRewards, usdToEthRate)
 
 	// Transformation of contributors map to contributors array
 	for _, contributor := range contributors {
@@ -81,4 +89,54 @@ func (gH *githubHandler) issuesToContributors(ctx context.Context, issues []*git
 	})
 
 	return contributorsArray, nil
+}
+
+// getEvents gets all events for an issue in a concurrent fashion.
+// TODO look through this with a fresh mind
+func (gH *githubHandler) getEvents(ctx context.Context, issues []*github.Issue, repoName string) (map[int64][]*github.IssueEvent, error) {
+	var (
+		events = eventContainer{
+			events: map[int64][]*github.IssueEvent{},
+		}
+		errChannel = make(chan error, len(issues))
+	)
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	wg := sync.WaitGroup{}
+
+	for _, issue := range issues {
+		wg.Add(1)
+		issue := issue
+		go func(ctx context.Context, repoName string, issueNumber int, issueID int64) {
+			defer wg.Done()
+
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				eventsResp, err := gH.githubInstallationClient.GetIssueEvents(ctx, repoName, issueNumber)
+				if err != nil {
+					log.Printf("[getEvents] error while getting events for issue with Number: %d, error: %v\n", issueNumber, err)
+					cancel()
+					errChannel <- err
+					return
+				}
+
+				events.add(issueID, eventsResp)
+			}
+		}(ctx, repoName, *issue.Number, *issue.ID)
+	}
+
+	wg.Wait()
+
+	// Checking for error
+	// TODO should we return multiple errors
+	select {
+	case err := <-errChannel:
+		return nil, err
+	default:
+	}
+
+	return events.events, nil
 }
