6ed49289f405127002c04aa6fb84e9f6e164c409
Kilian Käslin <kilian.kaeslin@googlemail.com>
2022-02-10 17:55:50 +0100

    - Refactored updateComments

--- internal/famed/commenthandler.go
+++ internal/famed/commenthandler.go
@@ -10,15 +10,10 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-type UpdateCommentsResponse struct {
-	mu            sync.Mutex
-	UpdatedIssues []int `json:"updatedIssues"`
-}
-
-func (UCR *UpdateCommentsResponse) append(i int) {
-	UCR.mu.Lock()
-	defer UCR.mu.Unlock()
-	UCR.UpdatedIssues = append(UCR.UpdatedIssues, i)
+type IssueCommentUpdate struct {
+	IssueNumber int    `json:"issueNumber"`
+	Updated     bool   `json:"updated"`
+	Error       string `json:"error"`
 }
 
 // UpdateComments updates the comments in a GitHub repo.
@@ -28,41 +23,44 @@ func (gH *githubHandler) UpdateComments(c echo.Context) error {
 		return echo.ErrBadRequest.SetInternal(ErrMissingRepoPathParameter)
 	}
 
-	response := UpdateCommentsResponse{UpdatedIssues: []int{}}
-	repo := NewRepo(gH.famedConfig, gH.githubInstallationClient, gH.currencyClient, repoName)
-
-	comments, err := repo.GetComments(c.Request().Context())
+	response, err := gH.checkAndUpdateComments(c.Request().Context(), repoName)
 	if err != nil {
 		return err
 	}
 
-	var wg sync.WaitGroup
-	for issueNumber, comment := range comments {
-		wg.Add(1)
-		go gH.checkAndUpdateComment(c.Request().Context(), &wg, repoName, issueNumber, comment, &response)
-	}
-
-	wg.Wait()
-
 	return c.JSON(http.StatusOK, response)
 }
 
-func getLastCommentsByUser(issueComments []*github.IssueComment, userID int64) *github.IssueComment {
-	for i := len(issueComments) - 1; i >= 0; i-- {
-		comment := issueComments[i]
+func (gH *githubHandler) checkAndUpdateComments(ctx context.Context, repoName string) ([]*IssueCommentUpdate, error) {
+	repo := NewRepo(gH.famedConfig, gH.githubInstallationClient, gH.currencyClient, repoName)
 
-		if comment.User != nil && *comment.User.ID == userID {
-			return comment
-		}
+	comments, err := repo.GetComments(ctx)
+	if err != nil {
+		return nil, err
 	}
-	return nil
+
+	issueCommentUpdates := make([]*IssueCommentUpdate, len(comments))
+	var wg sync.WaitGroup
+	i := 0
+	for issueNumber, comment := range comments {
+		issueCommentUpdates[i] = &IssueCommentUpdate{}
+		wg.Add(1)
+		go gH.checkAndUpdateComment(ctx, &wg, repoName, issueNumber, comment, issueCommentUpdates[i])
+		i++
+	}
+	wg.Wait()
+
+	return issueCommentUpdates, nil
 }
 
-func (gH *githubHandler) checkAndUpdateComment(ctx context.Context, wg *sync.WaitGroup, repoName string, issueNumber int, comment string, response *UpdateCommentsResponse) {
+func (gH *githubHandler) checkAndUpdateComment(ctx context.Context, wg *sync.WaitGroup, repoName string, issueNumber int, comment string, issueCommentUpdate *IssueCommentUpdate) {
 	defer wg.Done()
+	issueCommentUpdate.IssueNumber = issueNumber
+
 	issueComments, err := gH.githubInstallationClient.GetComments(ctx, repoName, issueNumber)
 	if err != nil {
 		log.Printf("[UpdateComments] error while getting comments for issue #%d, error: %v", issueNumber, err)
+		issueCommentUpdate.Error = err.Error()
 		return
 	}
 	lastCommentByBot := getLastCommentsByUser(issueComments, 96487857)
@@ -73,9 +71,22 @@ func (gH *githubHandler) checkAndUpdateComment(ctx context.Context, wg *sync.Wai
 		_, err := gH.githubInstallationClient.PostComment(ctx, repoName, issueNumber, comment)
 		if err != nil {
 			log.Printf("[UpdateComments] error while posting comment for issue #%d, error: %v", issueNumber, err)
+			issueCommentUpdate.Error = err.Error()
 			return
 		}
 
-		response.append(issueNumber)
+		issueCommentUpdate.Updated = true
 	}
+}
+
+func getLastCommentsByUser(issueComments []*github.IssueComment, userID int64) *github.IssueComment {
+	for i := len(issueComments) - 1; i >= 0; i-- {
+		comment := issueComments[i]
+
+		if comment.User != nil && *comment.User.ID == userID {
+			return comment
+		}
+	}
+
+	return nil
 }
