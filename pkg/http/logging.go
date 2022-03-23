13909648d5c450e1fcaad46ab9fead8fac4ab56f
Kilian Käslin <kilian.kaeslin@googlemail.com>
2022-03-23 18:32:15 +0100

    - Fixed rate limiting

--- pkg/http/logging.go
+++ pkg/http/logging.go
@@ -27,19 +27,23 @@ func (lRT loggingRoundTripper) RoundTrip(req *http.Request) (*http.Response, err
 	res, err := lRT.Proxied.RoundTrip(req)
 	receiveTime := time.Now()
 
-	errMsg := ""
-	if err != nil {
-		errMsg = err.Error()
-	}
-
 	msg := logMsg{
 		SendTime: sendTime.Format(time.RFC3339Nano),
 		Host:     req.Host,
 		Method:   req.Method,
-		Path:     req.URL.Path,
-		Status:   res.Status,
-		Error:    errMsg,
 		RTT:      fmt.Sprintf("%d ms", receiveTime.Sub(sendTime).Milliseconds()),
+	}
+
+	if err != nil {
+		msg.Error = err.Error()
+	}
+
+	if res != nil {
+		msg.Status = res.Status
+	}
+
+	if req.URL != nil {
+		msg.Path = req.URL.Path
 	}
 
 	bytes, err := json.Marshal(msg)
