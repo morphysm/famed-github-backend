d459eb867fc3e98be2bd1b894648b1de9125f8c7
Kilian Käslin <kilian.kaeslin@googlemail.com>
2022-04-04 12:09:41 +0200

    - Added red team folder structure

--- internal/famed/redTeam.go
+++ internal/famed/redTeam.go
@@ -2,6 +2,7 @@ package famed
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 
@@ -9,19 +10,38 @@ import (
 )
 
 func (gH *githubHandler) GetRedTeam(c echo.Context) error {
-	rawRedTeam, err := os.ReadFile("redTeam.json")
+	owner := c.Param("owner")
+	if owner == "" {
+		return echo.ErrBadRequest.SetInternal(ErrMissingOwnerPathParameter)
+	}
+
+	repoName := c.Param("repo_name")
+	if repoName == "" {
+		return echo.ErrBadRequest.SetInternal(ErrMissingRepoPathParameter)
+	}
+
+	redTeam, err := readRedTeamFromFile(owner, repoName)
 	if err != nil {
 		return err
+	}
+
+	return c.JSON(http.StatusOK, redTeam)
+}
+
+func readRedTeamFromFile(owner string, repo string) ([]*Contributor, error) {
+	path := fmt.Sprintf("redTeams/%s/%s/redTeam.json", owner, repo)
+
+	rawRedTeam, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
 	}
 
 	redTeam := Contributors{}
 	err = json.Unmarshal(rawRedTeam, &redTeam)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	redTeam.updateMonthlyRewards()
-	sortedRedTeam := redTeam.toSortedSlice()
-
-	return c.JSON(http.StatusOK, sortedRedTeam)
+	return redTeam.toSortedSlice(), nil
 }
