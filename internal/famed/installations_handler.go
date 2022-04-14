178e3d68a3f73c502ae7596d9e499c5ccadf7a1d
Kilian Käslin <kilian.kaeslin@googlemail.com>
2022-04-14 11:51:26 +0200

    - Added repos to installation call

--- internal/famed/installations_handler.go
+++ internal/famed/installations_handler.go
@@ -4,7 +4,13 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/morphysm/famed-github-backend/internal/client/github"
 )
+
+type installation struct {
+	github.Installation
+	Repos []string `json:"repositories"`
+}
 
 func (gH *githubHandler) GetInstallations(c echo.Context) error {
 	installations, err := gH.githubAppClient.GetInstallations(c.Request().Context())
@@ -12,5 +18,20 @@ func (gH *githubHandler) GetInstallations(c echo.Context) error {
 		return err
 	}
 
-	return c.JSON(http.StatusOK, installations)
+	resp := make([]installation, len(installations))
+	for i, instal := range installations {
+		repositories, err := gH.githubInstallationClient.GetRepos(c.Request().Context(), instal.Account.Login)
+		if err != nil {
+			return err
+		}
+
+		var repos []string
+		for _, repo := range repositories {
+			repos = append(repos, repo.Name)
+		}
+
+		resp[i] = installation{Installation: instal, Repos: repos}
+	}
+
+	return c.JSON(http.StatusOK, resp)
 }
