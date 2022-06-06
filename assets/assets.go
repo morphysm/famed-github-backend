ec15e5dc0c296d1ded578e13b4510150ce1c3b8c
GreyXor <greyxor@protonmail.com>
2022-06-06 22:42:48 +0200

    fix: correctly sized banner

--- assets/assets.go
+++ assets/assets.go
@@ -2,6 +2,6 @@ package assets
 
 import _ "embed"
 
-// http://patorjk.com/software/taag/#p=display&f=Small%20Slant&t=FamedBackend
+// Banner created with http://patorjk.com/software/taag/#p=display&f=Small%20Slant&t=FamedBackend
 //go:embed banner.txt
 var Banner string
