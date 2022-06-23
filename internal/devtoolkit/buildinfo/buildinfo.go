790cf62ae87b2ceb02bfce88e95a4df812555816
GreyXor <greyxor@protonmail.com>
2022-06-23 16:30:40 +0200

    ci: fix version from VCS

---
+++ internal/devtoolkit/buildinfo/buildinfo.go
@@ -0,0 +1,64 @@
+package buildinfo
+
+import (
+	"runtime"
+	"runtime/debug"
+	"time"
+
+	"github.com/coreos/go-semver/semver"
+	"github.com/rotisserie/eris"
+)
+
+const (
+	// ProjectName is the stylized project name.
+	ProjectName = "Famed"
+	// ProgramName is the program name (should be in ASCII character for wide portability).
+	ProgramName = "famed"
+	// ProjectWebsite is the project's web page.
+	ProjectWebsite = "https://www.famed.morphysm.com/"
+)
+
+var version = "0.0.0-dev"
+
+type BuildInfo struct {
+	Version         *semver.Version
+	Date            time.Time
+	Revision        string
+	Target          string
+	CompilerVersion string
+}
+
+func (i BuildInfo) UserAgent() string {
+	return ProgramName + "/" + i.Version.String() + " (" + runtime.Version() + "; " + i.Target + ")"
+}
+
+func NewBuildInfo() (*BuildInfo, error) {
+	buildInfo := &BuildInfo{
+		Target:          runtime.GOOS + "/" + runtime.GOARCH,
+		CompilerVersion: runtime.Version(),
+	}
+
+	// TODO: Retrieve version from vcs tag
+	version, err := semver.NewVersion(version)
+	if err != nil {
+		return nil, eris.Wrap(err, "failed to instantiate new semver")
+	}
+
+	buildInfo.Version = version
+
+	debugBuildInfo, ok := debug.ReadBuildInfo()
+	if !ok {
+		return nil, eris.Wrap(err, "failed to read build info")
+	}
+
+	for _, kv := range debugBuildInfo.Settings {
+		switch kv.Key {
+		case "vcs.revision":
+			buildInfo.Revision = kv.Value
+		case "vcs.time":
+			buildInfo.Date, _ = time.Parse(time.RFC3339, kv.Value)
+		}
+	}
+
+	return buildInfo, nil
+}
