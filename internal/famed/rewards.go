9c3edc2565526dc095453d6bb140ea36b85c3aee
Kilian Käslin <kilian.kaeslin@googlemail.com>
2022-07-08 17:15:40 +0200

    fix: fix no rewards if contributor generation return error

--- internal/famed/rewards.go
+++ internal/famed/rewards.go
@@ -65,9 +65,6 @@ func (gH *githubHandler) GetRewardsByOwner(c echo.Context) error {
 			rewardStructure := famedModel.NewRewardStructure(gH.famedConfig.Rewards, gH.famedConfig.DaysToFix, 2)
 			boardOptions := famedModel.NewBoardOptions(gH.famedConfig.Currency, rewardStructure, gH.now())
 			contributors, err := famedModel.NewBlueTeamFromIssue(enrichedIssue, boardOptions)
-			if err != nil {
-				return err
-			}
 
 			if len(contributors) != 0 && err == nil {
 				repoResp.Issues = append(repoResp.Issues, Issue{ID: issue.ID, Number: issue.Number, HTMLURL: issue.HTMLURL, Contributors: contributors})
@@ -75,6 +72,60 @@ func (gH *githubHandler) GetRewardsByOwner(c echo.Context) error {
 		}
 
 		response.Repos = append(response.Repos, repoResp)
+	}
+
+	return c.JSON(http.StatusOK, response)
+}
+
+// GetRewardsByContributor returns a list of rewards for a given contributor.
+func (gH *githubHandler) GetRewardsByContributor(c echo.Context) error {
+	var (
+		ctx         = c.Request().Context()
+		contributor = c.Param("contributor")
+	)
+
+	installations, err := gH.githubAppClient.GetInstallations(ctx)
+	if err != nil {
+		return err
+	}
+
+	response := EligibleIssuesResponse{Repos: []Repo{}}
+	for _, installation := range installations {
+		owner := installation.Account.Login
+		// TODO document drawback of contributor having to be assigned to issue
+		repos, err := gH.githubInstallationClient.GetReposByOwner(ctx, owner)
+		if err != nil {
+			return err
+		}
+
+		issueState := model.Closed
+		issueOptions := providers.IssueListByRepoOptions{
+			Labels:   []string{gH.famedConfig.Labels[config.FamedLabelKey].Name},
+			State:    &issueState,
+			Assignee: &contributor,
+		}
+		for _, repo := range repos {
+			repoResp := Repo{Name: repo, Issues: []Issue{}}
+			issues, err := gH.githubInstallationClient.GetIssuesByRepo(ctx, owner, repo, issueOptions)
+			if err != nil {
+				return err
+			}
+
+			for _, issue := range issues {
+				enrichedIssue := gH.githubInstallationClient.EnrichIssue(ctx, owner, repo, issue)
+
+				// TODO compress into githubHandler struct
+				rewardStructure := famedModel.NewRewardStructure(gH.famedConfig.Rewards, gH.famedConfig.DaysToFix, 2)
+				boardOptions := famedModel.NewBoardOptions(gH.famedConfig.Currency, rewardStructure, gH.now())
+				contributors, err := famedModel.NewBlueTeamFromIssue(enrichedIssue, boardOptions)
+
+				if len(contributors) != 0 && err == nil {
+					repoResp.Issues = append(repoResp.Issues, Issue{ID: issue.ID, Number: issue.Number, HTMLURL: issue.HTMLURL, Contributors: contributors})
+				}
+			}
+
+			response.Repos = append(response.Repos, repoResp)
+		}
 	}
 
 	return c.JSON(http.StatusOK, response)
